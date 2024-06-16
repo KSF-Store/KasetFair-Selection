073cac0fb605735195f7da04186ac098596a4e49
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-16 17:57:05 +0700

    Debugging docker unbehavior

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-50">
-      <div>Placeholder </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-400">
+      <div>
+        current page place holder  
+        qwgqegadga q akbla dg
+      </div>
     </main>
   )
 }
