a00dae2212567c622be4d3bcb1353f117a79b2be
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-17 12:33:10 +0700

    Add function get current user details

--- src/components/account/userDetail.tsx
+++ src/components/account/userDetail.tsx
@@ -1,32 +1,29 @@
 
 import React from 'react';
-import { auth } from '@/lib/authentication/auth';
-import { redirect } from 'next/navigation';
+
+
 import Image from 'next/image';
+
+import OnGetCurrentSession from '@/utils/getSession/getCurrentUser';
 
 type Props = {};
 
 export default async function UserDetail({}: Props) {
-    const session = await auth();
-    console.log(session)
-    if (!session?.user) {
-        // Redirect to sign-in page if the user is not logged in
-        return redirect('/');
-    }
+    const currentUser = await OnGetCurrentSession()
 
     return (
         <div className="flex items-center justify-center">
             <div className="bg-white shadow-lg rounded-lg p-10 max-w-sm w-full">
                 <div className="flex flex-col items-center">
                     <Image
-                        src={session.user.image || '/default-profile.png'} // fallback image
+                        src={currentUser.image || '/default-profile.png'} // fallback image
                         alt="User Profile"
                         className="w-24 h-24 rounded-full mb-4"
                         width={200}
                         height={200}
                     />
-                    <h2 className="text-2xl font-bold mb-2 text-center">{session.user.name}</h2>
-                    <p className="text-gray-700 mb-4 text-center">{session.user.email}</p>
+                    <h2 className="text-2xl font-bold mb-2 text-center">{currentUser.name}</h2>
+                    <p className="text-gray-700 mb-4 text-center">{currentUser.email}</p>
                 </div>
             </div>
         </div>
---
+++ src/utils/getSession/getCurrentUser.ts
@@ -0,0 +1,17 @@
+'use server'
+
+import { auth } from "@/lib/authentication/auth"
+
+import { redirect } from "next/navigation"
+
+export default async function OnGetCurrentSession(){
+    const session = await auth() 
+
+    if (!session?.user?.id){
+        //if user isn't existed
+        return redirect('/')
+    }
+    else {
+        return session.user
+    }
+}
