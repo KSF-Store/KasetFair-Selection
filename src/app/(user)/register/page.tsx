050e5c53d32e1b59f413dcf67bfa47eb626d7b23
ArKaRaShi <rnwz12345@gmail.com>
2024-06-17 16:00:31 +0700

    [KSF-6] Add register page, Add register component

---
+++ src/app/(user)/register/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Register from "@/components/register/register";
+
+export default function RegisterPage() {
+    return (
+        <>
+            <Register />
+        </>
+    );
+}
---
+++ src/components/register/register.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import OnGetCurrentSession from "@/utils/getSession/getCurrentUser";
+
+export default function Register() {
+    const [store, setStore] = useState({
+        storeName: "",
+        storeDescription: "",
+    });
+
+    const [loading, setLoading] = useState(false);
+    const onCreate = async () => {
+        try {
+            setLoading(true);
+            const user = await OnGetCurrentSession();
+            const body = { email: user.email, ...store };
+            const response = await fetch("/api/stores/store", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(body),
+            });
+            const data = await response.json();
+            console.log(data);
+        } catch (error: any) {
+            console.error("Error fetching session:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+    if (loading) {
+        return <h1>Loading...</h1>;
+    }
+
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="bg-white shadow-lg rounded-lg p-10">
+                <label className="self-start" htmlFor="name">
+                    Store name
+                </label>
+                <input
+                    className="mb-2 w-full rounded-lg border-l-black px-4 py-2"
+                    id="name"
+                    type="text"
+                    value={store.storeName}
+                    onChange={(e) => setStore({ ...store, storeName: e.target.value })}
+                    placeholder="store name"
+                ></input>
+                <label className="self-start" htmlFor="description">
+                    Store description
+                </label>
+                <input
+                    className="mb-4 w-full rounded-lg border-l-black px-4 py-2"
+                    id="description"
+                    type="text"
+                    value={store.storeDescription}
+                    onChange={(e) => setStore({ ...store, storeDescription: e.target.value })}
+                    placeholder="store description"
+                ></input>
+                <button
+                    onClick={onCreate}
+                    className="w-full bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded transition duration-300"
+                >
+                    Create store
+                </button>
+            </div>
+        </div>
+    );
+}
