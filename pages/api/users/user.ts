9fe3b8f650af2db697d36b408c2db9b46e13b008
ArKaRaShi <rnwz12345@gmail.com>
2024-06-17 13:29:08 +0700

    [KSF-12] Add user Post api

---
+++ pages/api/users/user.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { prismaDb } from "@/lib/prismaDb";
+import { auth } from "@/lib/authentication/auth";
+import type { Nisit } from "@prisma/client";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method === "POST") {
+        try {
+            const payload: Nisit = req.body;
+            const { email, name, role } = payload;
+
+            const existingUser = await prismaDb.nisit.findUnique({ where: { email } });
+            if (existingUser) {
+                return res.json({ message: "User exist", status: 400 });
+            }
+            const session = await auth();
+            if (!session) {
+                return res.json({ message: "No permission", status: 400 });
+            }
+
+            const newUser = await prismaDb.nisit.create({
+                data: {
+                    email,
+                    name,
+                    role,
+                },
+            });
+            return res.json({ data: newUser, message: "User saved", status: 200 });
+        } catch (error: any) {
+            res.json({ message: "Save user failed", status: 500 });
+        }
+    }
+}
