07d8ef0dee6026af45b0d68db6710d01931f47ab
ArKaRaShi <rnwz12345@gmail.com>
2024-06-18 15:38:21 +0700

    [KSF-13] Finish POST method

--- pages/api/users/user.ts
+++ pages/api/users/user.ts
@@ -1,23 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { prismaDb } from "@/lib/prismaDb";
-import { auth } from "@/lib/authentication/auth";
 import type { Nisit } from "@prisma/client";
+import { prismaDb } from "@/lib/prismaDb";
+import { getUserFromCookie } from "@/utils/jwtAccess/getToken";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === "POST") {
         try {
             const payload: Nisit = req.body;
             const { email, name, role } = payload;
-            console.log(email)
+
+            console.log(email, name, role);
             const existingUser = await prismaDb.nisit.findUnique({ where: { email } });
             if (existingUser) {
                 return res.json({ message: "User exist", status: 400 });
             }
-            const session = await auth();
-            if (!session) {
-                return res.json({ message: "No permission", status: 400 });
-            }
-
             const newUser = await prismaDb.nisit.create({
                 data: {
                     email,
