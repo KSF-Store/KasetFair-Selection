d22ba754dac722490a09042b4fd30b9fc9c5e613
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-25 23:17:19 +0700

    Init help page for users have details for stores that needs

---
+++ src/app/(user)/help/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+type Props = {}
+
+export default function page({}: Props) {
+  return (
+    <div>
+        All help and support for all user can see what are they selling types details slogan and more.
+        All documents need specific คู่มือ
+    </div>
+  )
+}
--- src/app/(user)/ksf-dashboard/page.tsx
+++ src/app/(user)/ksf-dashboard/page.tsx
@@ -6,6 +6,7 @@ export default function page({}: Props) {
   return (
     <div>
         All kaset fair dashboard for all user can see what are they selling types details slogan and more.
+        
     </div>
   )
 }
