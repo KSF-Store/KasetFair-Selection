98960c7a2a880d78afcb7d7ab99badf397ee6adc
ArKaRaShi <rnwz12345@gmail.com>
2024-06-26 23:59:06 +0700

    [KSF-14] Add register user when oauth(mockup)

--- src/server/connectToDb/AddUser.ts
+++ src/server/connectToDb/AddUser.ts
@@ -1,21 +1,29 @@
+import axios from "axios";
+
 export default async function OnAddUserToDb(user: any) {
     const baseUrl = process.env.NEXTAUTH_URL || "http://localhost:3000";
 
     try {
-        const response = await fetch(`${baseUrl}/api/users`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                email: user?.email,
-                name: user?.name,
-                role: user?.role,
-            }),
-        });
-        if (!response.ok) {
-            throw new Error("Something bad happen in OnAddUserToDb");
-        }
+        const mockup = {
+            nisitId: "6510503565",
+            role: "std",
+            name: "Ponthep Ho",
+            faculty: "ComEn",
+            year: 3,
+            address: "Chatuchak Bangkok",
+            phone: "0912345678",
+            reservePhone1: "0987654321",
+        };
+        const body = mockup;
+        // const body = {
+        //     email: user?.email,
+        //     name: user?.name,
+        //     role: user?.role,
+        // };
+        const response = await axios.post(`${baseUrl}/api/user`, body);
+        console.log(response);
+        // if (response.) {
+        //     throw new Error("Something bad happen in OnAddUserToDb");
 
         // console.log(await response.json());
     } catch (error: any) {
