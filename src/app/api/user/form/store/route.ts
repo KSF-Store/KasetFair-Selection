93fee39cc106deead9441a6ab202efdb821b799c
ArKaRaShi <rnwz12345@gmail.com>
2024-06-27 00:04:19 +0700

    [KSF-14] Add POST, PUT user/store, Add POST, GET[init] user

--- src/app/api/user/form/store/route.ts
+++ src/app/api/user/form/store/route.ts
@@ -2,43 +2,107 @@ import { NextRequest, NextResponse } from "next/server";
 import { prismaDb } from "@/lib/prismaDb";
 
 export async function POST(req: NextRequest) {
-    
-
-
     try {
-        const store = await prismaDb.store.create({
-          data: {
+        const {
             storeRole,
             name,
             mainProductType,
             subProductType,
             innovation,
-            status,
             ownerId,
-            isAssigned: false,  // default value
-          },
+        } = await req.json();
+
+        const store = await prismaDb.store.create({
+            data: {
+                storeRole,
+                name,
+                mainProductType,
+                subProductType,
+                innovation,
+                status: 1,
+                ownerId,
+            },
         });
+        return NextResponse.json(
+            {
+                data: store,
+                message: "Create store succesful",
+            },
+            { status: 201 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: "Create store failed" },
+            { status: 500 }
+        );
+        // handle
+        // .
+        // .
     }
-    catch(error:any) {
-        console.log('ee')
-    }
-
-
-
-    // userId          Int       @id @default(autoincrement())
-    // nisitId         String    @unique
-    // name            String
-    // faculty         String
-    // year            Int
-    // address         String
-    // phone           String    
-    // reservePhone1   String?
-    // reservePhone2   String?
-    // storeId         Int?
-    // Store           Store?    @relation(fields: [storeId], references: [storeId])
-  }
 }
 
-export async function PUT() {
-    
+export async function PUT(req: NextRequest) {
+    try {
+        const {
+            userId,
+            storeRole,
+            name,
+            mainProductType,
+            subProductType,
+            innovation,
+            ownerId,
+        } = await req.json();
+
+        if (userId != ownerId) {
+            return NextResponse.json(
+                { message: "No access to update store" },
+                { status: 400 }
+            );
+        }
+
+        const user = await prismaDb.user.findUnique({
+            where: { userId },
+            include: { Store: true },
+        });
+        if (!user) {
+            return NextResponse.json(
+                { message: "User not existd" },
+                { status: 404 }
+            );
+        }
+
+        const store = user.Store;
+        if (!store) {
+            return NextResponse.json(
+                { message: "Store not existd" },
+                { status: 404 }
+            );
+        }
+
+        const updatedStore = await prismaDb.store.update({
+            where: {
+                storeId: store.storeId,
+            },
+            data: {
+                storeRole: storeRole || store.storeRole,
+                name: name || store.name,
+                mainProductType: mainProductType || store.mainProductType,
+                subProductType: subProductType || store.subProductType,
+                innovation: innovation || store.innovation,
+                ownerId: ownerId || store.ownerId,
+            },
+        });
+        return NextResponse.json(
+            { data: updatedStore, message: "Update store succesful" },
+            { status: 200 }
+        );
+    } catch (error: any) {
+        return NextResponse.json(
+            { message: "Update store failed" },
+            { status: 500 }
+        );
+        // handle
+        // .
+        // .
+    }
 }
