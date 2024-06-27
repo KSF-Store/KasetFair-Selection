b0a989ec8f06d1e7ed5e19f748dbe6c402747d6f
ArKaRaShi <rnwz12345@gmail.com>
2024-06-28 00:16:31 +0700

    [KSF-14] Config Payload Type

--- src/interface/payloadType.ts
+++ src/interface/payloadType.ts
@@ -1,6 +1,14 @@
 import { StoreType, UserType } from "./dbType";
 
+export type StorePayload = Omit<
+    StoreType,
+    "Sdg" | "Booth" | "boothId" | "isAssigned" | "Member"
+> & {
+    invitingNisitId: string[];
+    sdgId: number[];
+};
+
 export interface StoreEditPayload {
-    User : UserType ; 
-    Store : StoreType ; 
+    User: UserType;
+    Store: StorePayload;
 }
