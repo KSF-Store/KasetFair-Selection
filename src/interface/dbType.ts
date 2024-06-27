f0bf54e592d6e5d84f5f50574a964f1aad5f3405
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-27 11:42:19 +0700

    ADding interface sync to prisma model

--- src/interface/dbType.ts
+++ src/interface/dbType.ts
@@ -1,5 +1,36 @@
-
-import { Nisit as PrismaNisit, NisitStore as PrismaNisitStore } from '@prisma/client';
-
-export type NisitType = PrismaNisit;
-export type NisitStoreType = PrismaNisitStore;
+export interface User {
+    userId: number;
+    nisitId: string;
+    role: string;
+    name: string;
+    faculty: string;
+    year: number;
+    address: string;
+    phone: string;
+    reservePhone1?: string;
+    reservePhone2?: string;
+    storeId?: number;
+    Store?: Store;
+  }
+  
+  export interface Store {
+    storeId: number;
+    storeRole: string;
+    name: string;
+    mainProductType: string;
+    subProductType?: string;
+    innovation?: string;
+    status: number;  // 0 for draft, 1 for pending ...
+    ownerId: number;
+    Member?: User[];
+    isAssigned: boolean;
+    boothId?: number;
+    Booth?: Booth;
+  }
+  
+  export interface Booth {
+    id: number;
+    name: string;
+    isAssigned: boolean;
+    Store?: Store;
+  }
