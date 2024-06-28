e4d554e2a8d56fa7e85456baa36cf3c21fec8978
ArKaRaShi <rnwz12345@gmail.com>
2024-06-28 20:34:24 +0700

    [KSF-14] Change StorePayload attrs. can be null

--- src/interface/payloadType.ts
+++ src/interface/payloadType.ts
@@ -4,8 +4,9 @@ export type StorePayload = Omit<
     StoreType,
     "Sdg" | "Booth" | "boothId" | "isAssigned" | "Member"
 > & {
-    invitingNisitId: string[];
-    sdgId: number[];
+    memberUserId?: number[];
+    invitingNisitId?: string[];
+    sdgId?: number[];
 };
 
 export type UserPayload = UserType;
