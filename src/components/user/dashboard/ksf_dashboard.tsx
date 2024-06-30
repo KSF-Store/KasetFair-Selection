19f45f161e4048078f7c858e6bb3d3b0a12f8547
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-06-30 12:17:20 +0700

    Re routes for all store list case doesn't  have yo login case see what selling this year

--- src/components/user/dashboard/ksf_dashboard.tsx
+++ src/components/user/dashboard/ksf_dashboard.tsx
@@ -5,7 +5,10 @@ type Props = {}
 export default function AllStoreDashboard({}: Props) {
   return (
     <section>
-
+        <h1>Store Dashboard</h1>
+        <input 
+            placeholder="Search all store comppettion or store details here"
+        />
     </section>
   )
 }
